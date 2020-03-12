da346952f0d53183cb1de159238a00093167d35d
Pascal Benchimol <pascal.benchimol@kisio.com>
2020-03-12 11:15:07 +0100

    before specializing path tree

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,64 @@
-
 #[allow(dead_code)]
 
 
 trait PublicTransit {
-    type StopPoint : Clone;
 
+
+    // A route is a sequence of stop points
     type Route : Clone;
+    // A stop point in a route, along with its position in this route
+    type RouteStop : Clone;
 
+    // A trip of a vehicle along a route
     type Trip;
-    type TripStop;
 
-    type Transfer;
 
     type Criteria : Clone;
 
     fn is_lower(&self, lower : & Self::Criteria, upper : & Self::Criteria) -> bool;
 
-    type Routes : Iterator<Item = Self::Route>;
-    fn routes(&self, stop_point : & Self::StopPoint) -> Self::Routes;
 
-    // returns true if upstream is positionned strictly before downstream in  route
-    // panics if upstream or downstream is not in route
-    fn is_upstream(&self, route : & Self::Route, upstream : & Self::StopPoint, downstream : & Self::StopPoint) -> bool;
 
-    fn embark(route : & Self::Route, stop_point : & Self::StopPoint, criteria : Self::Criteria) -> (Self::TripStop, Self::Criteria);
+    // returns true if upstream is positionned strictly before downstream in their common Route
+    // panics if upstream and downstream are not in the same Route
+    fn is_upstream(&self,  upstream : & Self::RouteStop, downstream : & Self::RouteStop) -> bool;
 
-    type Ride : Iterator<Item = (Self::TripStop, Self::Criteria)>;
-    fn ride(trip_stop : Self::TripStop, criteria : Self::Criteria) -> Self::Ride;
 
-    fn stop_point_of(trip_stop : Self::TripStop) -> Self::StopPoint;
-    fn trip_of(trip_stop : Self::TripStop) -> Self::Trip;
-    fn route_of(trip : Self::Trip) -> Self::Route;
+    fn board(&self,
+             route_stop : & Self::RouteStop, 
+             criteria : & Self::Criteria
+            ) -> (Self::Trip, Self::Criteria);
 
-    type Transfers : Iterator<Item = (Self::Transfer, Self::Criteria)>;
-    fn transfers(departure : Self::StopPoint, start : Self::Criteria) -> Self::Transfers;
+    fn alight(&self,
+              trip : & Self::Trip,
+              route_stop : & Self::RouteStop, 
+              criteria : & Self::Criteria
+             ) -> (Self::Trip, Self::Criteria);
 
-    fn start_of(transfer : Self::Transfer) -> Self::StopPoint;
-    fn end_of(transfer : Self::Transfer) -> Self::StopPoint;
 
-    type JourneyDepartures : Iterator<Item = (Self::StopPoint, Self::Criteria)>;
+    fn next_on_route(&self, route_stop : & Self::RouteStop) -> Option<Self::RouteStop>;
+
+    fn ride(trip : & Self::Trip, 
+            route_stop : & Self::RouteStop,
+            criteria : & Self::Criteria
+            ) -> Option<Self::Criteria>;
+
+    fn route_of(&self, route_stop : & Self::RouteStop) -> Self::Route;
+
+    type Transfers : Iterator<Item = (Self::RouteStop, Self::Criteria)>;
+    fn transfers(departure : Self::RouteStop, start : Self::Criteria) -> Self::Transfers;
+
+
+
+    type JourneyDepartures : Iterator<Item = (Self::RouteStop, Self::Criteria)>;
     fn journey_departures(&self) -> Self::JourneyDepartures;
-    fn journey_arrival(stop_point : Self::StopPoint, criteria : Self::Criteria) -> Self::Criteria;
+    fn journey_arrival(route_stop : & Self::RouteStop, 
+                        criteria : & Self::Criteria
+                      ) -> Option<Self::Criteria>; //Returns None if destination is not reachable from route_stop
 
-    fn nb_of_stop_points(&self) -> usize;
-    //returns an usize between 0 and nb_of_stop_points()
-    fn stop_point_id(&self, stop_point : & Self::StopPoint) -> usize;
+    fn nb_of_route_stops(&self) -> usize;
+    //returns an usize between 0 and nb_of_route_stops()
+    fn route_stop_id(&self, route_stop : & Self::RouteStop) -> usize;
 
     fn nb_of_routes(&self) -> usize;
     fn route_id(&self, route : & Self::Route) -> usize;
@@ -91,10 +105,11 @@ impl<Data> PathTree<Data> {
 }
 
 enum JourneyLeg<Pb : PublicTransit> {
-    Departure(Pb::StopPoint),               // leg from the departure point to a public transit stop point
-    Transit(Pb::TripStop, Pb::TripStop),    // a ride of a public transit Trip between two TripStop
-    Transfer(Pb::Transfer),                 //a transfer between two stop point
-    Arrival(Pb::StopPoint)                  // leg from a public transit stop point to the arrival
+    Departure(Pb::RouteStop),               // leg from the departure point to a public transit stop point
+    Board(Pb::Trip, Pb::RouteStop),
+    Alight(Pb::Trip, Pb::RouteStop),
+    Transfer(Pb::RouteStop, Pb::RouteStop), //a transfer between two stop point
+    Arrival(Pb::RouteStop)                  // leg from a public transit stop point to the arrival
 }
 
 struct PathData<Pb : PublicTransit> {
@@ -135,59 +150,84 @@ impl<Pb : PublicTransit> ParetoFront<Pb> {
         });
         self.elements.push((path.clone(), criteria.clone()));
     }
+
+    fn merge_with(& mut self, other : & Self, pb : & Pb) {
+        for element in & other.elements {
+            let path = &element.0;
+            let criteria = &element.1;
+            self.add(path, criteria, pb);
+        }
+    }
 }
 
 #[allow(dead_code)]
 fn compute<Pb : PublicTransit>(pb : & Pb) -> () {
     let mut path_tree = PathTree::<PathData<Pb>>::new();
 
-    let nb_of_stop_points = pb.nb_of_stop_points();
-    // map a stop_point_id to the pareto front of Pathes which
-    // ends at stop_point with a Transit 
-    let mut alight_pareto_fronts = vec![ParetoFront::<Pb>::new(); nb_of_stop_points];
-    // map a stop_point_id to the pareto front of Pathes which
-    // ends at stop_point with a Transfer or a Departure 
-    let mut board_pareto_fronts = vec![ParetoFront::<Pb>::new(); nb_of_stop_points];
+    let nb_of_route_stops = pb.nb_of_route_stops();
+    // map a route_stop to the pareto front of Pathes which
+    // ends at route_stop with a Transit 
+    let mut alight_pareto_fronts = vec![ParetoFront::<Pb>::new(); nb_of_route_stops];
+    // map a route_stop to the pareto front of Pathes which
+    // ends at route_stop with a Transfer or a Departure 
+    let mut board_pareto_fronts = vec![ParetoFront::<Pb>::new(); nb_of_route_stops];
 
     let nb_of_routes = pb.nb_of_routes();
     
-    let mut route_has_a_new_board_path = vec![None; nb_of_routes];
+    let mut route_has_a_new_board_path : Vec::<Option<Pb::RouteStop>> = vec![None; nb_of_routes];
     let mut routes_with_new_board_path : Vec::<Pb::Route> = Vec::new();
 
     let root = path_tree.root();
-    for (stop_point, criteria) in pb.journey_departures() {
+    for (route_stop, criteria) in pb.journey_departures() {
         let path_data = PathData {
-            journey_leg :  JourneyLeg::Departure(stop_point.clone()) ,
+            journey_leg :  JourneyLeg::Departure(route_stop.clone()) ,
             criteria : criteria.clone()
         };
         let path = path_tree.extend(&root, path_data);
-        let stop_point_id = pb.stop_point_id(&stop_point);
-        board_pareto_fronts[stop_point_id].add(&path, &criteria, pb);
+        let route_stop_id = pb.route_stop_id(&route_stop);
+        board_pareto_fronts[route_stop_id].add(&path, &criteria, pb);
         
-        for route in pb.routes(&stop_point) {
-            let route_id = pb.route_id(&route);
-            if let Some(old_board_point) = &route_has_a_new_board_path[route_id] {
-                if pb.is_upstream(&route, &stop_point, old_board_point) {
-                    route_has_a_new_board_path[route_id] = Some(stop_point.clone());
-                }
-            }
-            else {
-                route_has_a_new_board_path[route_id] = Some(stop_point.clone());
-                routes_with_new_board_path.push(route);
-            }
+        let route = pb.route_of(&route_stop);
 
+        let route_id = pb.route_id(&route);
+        if let Some(old_board_point) = &route_has_a_new_board_path[route_id] {
+            if pb.is_upstream(&route_stop, old_board_point) {
+                route_has_a_new_board_path[route_id] = Some(route_stop.clone());
+            }
         }
+        else {
+            route_has_a_new_board_path[route_id] = Some(route_stop.clone());
+            routes_with_new_board_path.push(route);
+        }
+
+        
 
     }
 
     while ! routes_with_new_board_path.is_empty() {
         for route in & routes_with_new_board_path {
             let route_id = pb.route_id(route);
-            let board_stop_point = route_has_a_new_board_path[route_id].clone().unwrap();
-            let board_stop_point_id = pb.stop_point_id(&board_stop_point);
-            let pareto_front = board_pareto_fronts[board_stop_point_id].clone();
-            for (path, criteria) in & pareto_front.elements {
+            let mut has_stop = route_has_a_new_board_path[route_id].clone();
+            let mut pareto_front = ParetoFront::new();
+            while let Some(ref stop) = has_stop {
+                let stop_id = pb.route_stop_id(&stop);
+                // update alighting pareto front at this stop with elements from
+                //   "working" pareto front
 
+                let local_aligth_pathes = & mut alight_pareto_fronts[stop_id];
+                local_aligth_pathes.merge_with(&pareto_front, pb);
+
+                // update "working" pareto front with boarding pathes
+                let local_board_pathes = board_pareto_fronts[stop_id].clone();
+                pareto_front.merge_with(&local_board_pathes, pb);
+
+                // ride to the next stop point and update "working"
+                //   pareto front along the way
+
+
+
+                // go to next route stop and iterate
+                has_stop = pb.next_on_route(stop);
             }
 
         }
