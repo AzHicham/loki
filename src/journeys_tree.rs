d759374ea3595775cd3f4b520eca4205e3754fb3
Pascal Benchimol <pascal.benchimol@kisio.com>
2020-03-12 12:22:26 +0100

    journey tree version 2

--- src/journeys_tree.rs
+++ src/journeys_tree.rs
@@ -6,10 +6,6 @@ type Id = usize;
 
 const MAX_ID : Id = std::usize::MAX;
 
-#[derive(Clone, Copy, Debug)]
-struct Departure {
-    id : Id
-}
 
 #[derive(Clone, Copy, Debug)]
 struct Board {
@@ -31,19 +27,15 @@ struct Arrival {
     id : Id
 }
 
-enum BoardParent{
-    Departure(Departure),
-    Transfer(Transfer),
-}
+
 
 /// A complete journey is a sequence of the form
-///  Departure, (Board, Aligth, Transfer)* , Board, Aligth, Arrival
-/// i.e. it always starts with a Departure, followed by zero or more (Board, Aligth, Transfer)
+///  Transfer, (Board, Aligth, Transfer)* , Board, Aligth, Arrival
+/// i.e. it always starts with a Transfer, followed by zero or more (Board, Aligth, Transfer)
 ///      and then finished by a Board, Aligth, Arrival
 /// 
 /// We associate the minimum amount of data to each leg type so as to be able to reconstruct
 /// the whole journey :
-///  - Departure -> a RouteStop 
 ///  - Board     -> a Trip  
 ///      the specific RouteStop at which this Trip is boarded is given by the RouteStop
 ///      associated to the leg before the Board : either a Departure or a Transfer
@@ -51,7 +43,9 @@ enum BoardParent{
 ///      the specific RouteStop where we alight. The specific Trip that is alighted is
 ///      given by the Trip associated to the Board leg that comes before this Alight
 ///  - Transfer  -> a RouteStop
-///      the specific RouteStop where the Transfer ends. The specific RouteStop at which
+///      the specific RouteStop where the Transfer ends. 
+///      If this Transfer is not the beginning of the journey (i.e. the Departure leg), 
+///      the specific RouteStop at which
 ///      this Transfer ends is given by the RouteStop associated to the Aligth leg
 ///      that comes before this Transfer
 ///  - Arrival -> nothing
@@ -60,16 +54,16 @@ enum BoardParent{
 
 struct JourneysTree<PT : PublicTransit> {
     // data associated to each leg
-    departures : Vec<PT::RouteStop>,  
     boards     : Vec<PT::Trip>,
     alights    : Vec<PT::RouteStop>,
     transfers  : Vec<PT::RouteStop>,
 
     // parents 
+    board_parents   : Vec<Transfer>,
     alight_parents  : Vec<Board>,
-    transfer_parents : Vec<Alight>,
+    transfer_parents : Vec<Option<Alight>>,
     arrival_parents : Vec<Alight>,
-    board_parents   : Vec<BoardParent>,
+
 
 }
 
@@ -77,39 +71,65 @@ impl<PT : PublicTransit> JourneysTree<PT> {
 
     fn new() -> Self {
         Self {
-            departures : Vec::new(),
             boards : Vec::new(),
             alights : Vec::new(),
             transfers : Vec::new(),
 
+            board_parents    : Vec::new()
             alight_parents   : Vec::new(),
             transfer_parents : Vec::new(),
             arrival_parents  : Vec::new(),
-            board_parents    : Vec::new()
+
         }
     }
 
+    fn depart(& mut self, route_stop : & PT::RouteStop) -> Transfer {
+        debug_assert!(self.transfers.len() < MAX_ID);
+        debug_assert!(self.transfers.len() == self.transfer_parents.len());
+        let id = self.transfers.len();
+        self.transfers.push(route_stop.clone());
+        self.transfer_parents.push(None);
 
-    fn extend(& mut self, path : & Path, data : Data) -> Path {
-        assert!(self.parents.len() < MAX_PATH_ID);
-        let result = Path{ id : self.parents.len() };
-        self.parents.push(*path);
-        self.datas.push(data);
-
-        result
+        Transfer{ id }
     }
-}
 
-enum JourneyLeg<Pb : PublicTransit> {
-    Departure(Pb::RouteStop),               // leg from the departure point to a public transit stop point
-    Board(Pb::Trip, Pb::RouteStop),
-    Alight(Pb::Trip, Pb::RouteStop),
-    Transfer(Pb::RouteStop, Pb::RouteStop), //a transfer between two stop point
-    Arrival(Pb::RouteStop)                  // leg from a public transit stop point to the arrival
-}
 
-struct PathData<Pb : PublicTransit> {
-    journey_leg : JourneyLeg<Pb>,       // the last journey leg on this path
-    criteria : Pb::Criteria         // value of the Criteria at the end of this path
+    fn board(& mut self, transfer : & Transfer, trip : & PT::Trip) -> Board {
+        debug_assert!(self.boards.len() < MAX_ID);
+        debug_assert!(self.boards.len() == self.board_parents.len());
+        let id = self.boards.len();
+        self.boards.push(trip.clone());
+        self.board_parents.push(transfer.clone());
+
+        Board{ id }
+    }
+
+    fn alight(& mut self, board : & Board, route_stop : & PT::RouteStop) -> Alight {
+        debug_assert!(self.aligths.len() < MAX_ID);
+        debug_assert!(self.alights.len() == self.alight_parents.len());
+        let id = self.alights.len();
+        self.alights.push(route_stop.clone());
+        self.alight_parents.push(board.clone());
+
+        Alight{ id }
+    }
+
+    fn transfer(& mut self, alight : & Alight, route_stop : & PT::RouteStop) -> Transfer {
+        debug_assert!(self.transfers.len() < MAX_ID);
+        debug_assert!(self.transfers.len() == self.transfer_parents.len());
+        let id = self.transfers.len();
+        self.transfers.push(route_stop.clone());
+        self.transfer_parents.push(alight.clone());
+
+        Transfer{ id }
+    }
+
+    fn arrive(& mut self, alight : & Alight) -> Arrival {
+        debug_assert!(self.arrival_parents.len() < MAX_ID);
+        let id = self.arrival_parents.len();
+        self.arrival_parents.push(alight.clone());
+
+        Arrival{ id }
+    }
 }
 
