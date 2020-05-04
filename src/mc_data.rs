f4011922f45f64f1f264ff27bfc0158c92aebfcc
Pascal Benchimol <pascal.benchimol@kisio.com>
2020-05-04 13:44:14 +0200

    wip

--- src/mc_data.rs
+++ src/mc_data.rs
@@ -18,56 +18,57 @@ fn run() {
 // TODO : group collections.vehicle_journeys by mission (same sequence of stop_points)
 //    info : each VehicleJourney.stop_time is sorted by stop_time.sequence in ntfs::read::manage_stop_times
 
+
 struct Route {
-    missions : Vec<Mission>,
+    id : usize
 }
 
 struct Mission {
-    stops : Vec<Idx<StopPoint>>,
-    trips : Vec<Trip>,
-}
-
-struct Stop {
-    uri : String
+    route : Route,
+    id : usize,
 }
 
 struct Trip {
+    mission : Mission,
+    id : usize
+}
+
+struct Stop {
+    id : usize
+}
+
+
+
+struct RouteData {
+    missions : Vec<MissionData>,
+}
+
+struct MissionData {
+    stops : Vec<Stop>,
+    trips : Vec<TripData>,
+}
+
+struct TripData {
     arrival_times : Vec<Time>,
     departure_times : Vec<Time>,
     transit_model_vehicle_journey : Idx<VehicleJourney>,
 }
 
-// /
-// / stop_points_with_transit_to_explore : List<StopPoint>
-// / init stop_points_with_transit_to_explore with reachable stop_points
-// / while stop_points_with_transit_to_explore is not empty {
-// /    paths_by_stop_points : Map<StopPoint, ParetoFront<Path> >
-// /    for stop_point : stop_points_with_transit_to_explore {
-// /        stop_points_with_transfers_to_explore : List<StopPoint>
-// /        for path in paths_by_stop_points(stop_point) {
-// /            old_objective = path.objective
-// /            for route in routes(stop_point) {
-// /                (vehicle_journey, objective) = embark(stop_point, old_objective)
-// /                for (new_stop_point, new_objective) in ride(vehicle_journey, objective) {
-// /                    old_pareto_front = paths_by_stop_points(new_stop_point)
-// /                    // update old_pareto_front with a new_path with new_objective
-// /                    // if old_pareto_front is updated : 
-// /                        add new_stop_point to stop_points_with_a_transfer_to_explore
-// /                }
-// /            }
-// /        }
-// /    }
-// /    for stop_point : stop_points_with_transfers_to_explore {
-// /        perform all transfer from stop_point
-// /        update pareto front at arrival
-// /        push arrival_stop_point to stop_points_with_transit_to_explore if pareto front has been updated
-// /    }
-// / }
-// / 
-// / path : sequence of (StopPoint, Trip, Transfer)
-// / 
-// / 
-// / 
-// / 
+struct StopData {
+    transit_model_id : Idx<StopPoint>,
+    boardable_missions : Vec<Mission>,
+    position_in_mission : Vec<(Mission, usize)>,
+    transfers : Vec<(Stop, Time)>
+}
 
+struct MCData {
+    routes : Vec<RouteData>,
+    stops : Vec<StopData>,
+}
 
+// read all stops and put them in MCData.stops
+// read all transfers, and fill MCData.stops.transfers 
+
+// read all vehicle_journeys to identify mission patterns 
+//   based on sequence of stop_point_idx in vj.stop_times
+// read again all vehicle_journeys and fill MCData.missions with a new tripdata for each vj
