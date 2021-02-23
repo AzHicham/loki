9cc26488dbf2b4dc998df0ca43d117d80a38a8fe
Pascal Benchimol <pascal.benchimol@kisio.com>
2021-02-23 16:31:36 +0100

    fix sockets so that all components can communicate

--- docker/bina.sh
+++ docker/bina.sh
@@ -3,6 +3,7 @@ set -e
 
 dir="/data"
 
+
 # tranform gtfs into ntfs
 # gtfs2ntfs --input $dir/gtfs --output $dir/ntfs
 
@@ -29,29 +30,36 @@ jq -n --arg instance "${instance}_kraken" --arg krakenSocket "${krakenSocket}" '
     zmq_socket: $krakenSocket
 }'  > $dir/jormun_conf/$instance.json
 # one for "laxatips" with loads criteria
-jq -n --arg instance "${instance}_laxatips_loads" --arg krakenSocket "${krakenSocket}" --arg laxatipsSocket "$loadsSocket" '{ 
+jq -n --arg instance "${instance}_laxatips_loads" --arg krakenSocket "${krakenSocket}" --arg laxatipsSocket "${loadsSocket}" '{ 
     key: $instance, 
     zmq_socket: $krakenSocket, 
-    pt_zmq_socket : "$socket" 
+    pt_zmq_socket : $laxatipsSocket 
 }'  > $dir/jormun_conf/${instance}_loads.json
 # one for "laxatips" with classic criteria
-jq -n --arg instance "${instance}_laxatips_loads" --arg krakenSocket "${krakenSocket}" --arg laxatipsSocket "$classicSocket" '{ 
+jq -n --arg instance "${instance}_laxatips_classic" --arg krakenSocket "${krakenSocket}" --arg laxatipsSocket "${classicSocket}" '{ 
     key: $instance, 
     zmq_socket: $krakenSocket, 
-    pt_zmq_socket : "$laxatipsSocket" 
+    pt_zmq_socket : $laxatipsSocket 
 }'  > $dir/jormun_conf/${instance}_classic.json
 
 # kraken config file
-echo "{[GENERAL]
+echo "[GENERAL]
 instance_name = ${instance}_kraken
 database = /data/data.nav.lz4
-zmq_socket = $krakenSocket " > $dir/kraken.ini
+zmq_socket = $krakenSocket 
+
+[BROKER]
+host = rabbitmq
+port = 5672
+username = guest
+password = guest
+" > $dir/kraken.ini
 
 # Laxatips config files
 # one for the coverage with loads criteria
 jq -n --arg laxatipsSocket "$loadsSocket" '{
   ntfs_path: "/data/ntfs/",
-  loads_data_path: "/data/stoptime_loads.csv",
+  loads_data_path: "/data/stoptimes_loads.csv",
   socket: $laxatipsSocket,
   request_type: "loads",
   implem: "loads_periodic"
@@ -59,9 +67,10 @@ jq -n --arg laxatipsSocket "$loadsSocket" '{
 # one for the coverage with classic criteria
 jq -n --arg laxatipsSocket "$classicSocket"  '{
   ntfs_path: "/data/ntfs/",
-  loads_data_path: "/data/stoptime_loads.csv",
+  loads_data_path: "/data/stoptimes_loads.csv",
   socket: $laxatipsSocket,
   request_type: "classic",
   implem: "periodic"
 }' > $dir/laxatips_conf/classic.json
 
+chmod -R 777 $dir
