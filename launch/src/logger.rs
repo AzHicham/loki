df226cd7ac3549d295d9d0f9c8eda66af0fd589b
Hicham Azimani <hicham.azimani@kisio.com>
2021-09-20 17:18:02 +0200

    parse RUST_LOG or set INFO_LEVEL

--- launch/src/logger.rs
+++ launch/src/logger.rs
@@ -1,9 +1,23 @@
+use crate::loki::tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
 
 pub fn init_logger() {
+    let default_level = LevelFilter::INFO;
+    let rust_log =
+        std::env::var(EnvFilter::DEFAULT_ENV).unwrap_or_else(|_| default_level.to_string());
+    let env_filter_subscriber = EnvFilter::try_new(rust_log).unwrap_or_else(|err| {
+        eprintln!(
+            "invalid {}, falling back to level '{}' - {}",
+            EnvFilter::DEFAULT_ENV,
+            default_level,
+            err,
+        );
+        EnvFilter::new(default_level.to_string())
+    });
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::filter::EnvFilter::from_default_env())
-        .init()
+        .with(env_filter_subscriber)
+        .init();
 }
