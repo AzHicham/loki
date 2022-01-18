e0f9868a4f2e27f7a1f3317b0cd6edba66c6e556
Pascal Benchimol <pascal.benchimol@kisio.com>
2022-01-18 18:31:36 +0100

    do not stop the worker when an error occurs in request handling

--- server/src/compute_worker.rs
+++ server/src/compute_worker.rs
@@ -114,28 +114,32 @@ impl ComputeWorker {
 
             info!("Worker {} received a request.", self.worker_id.id);
 
-            let proto_response = self
-                .handle_request(request_message.payload)
-                .with_context(|| format!("Compute worker {} will stop.", self.worker_id.id))?;
+            let reponse_result = self.handle_request(request_message.payload);
+            match reponse_result {
+                Err(err) => {
+                    error!("An error occured while solving a request : {:#?}", err);
+                }
+                Ok(proto_response) => {
+                    let response_message = ResponseMessage {
+                        payload: proto_response,
+                        client_id: request_message.client_id,
+                    };
 
-            let response_message = ResponseMessage {
-                payload: proto_response,
-                client_id: request_message.client_id,
-            };
+                    debug!("Worker {} finished solving.", self.worker_id.id);
 
-            debug!("Worker {} finished solving.", self.worker_id.id);
+                    // block until the response is sent
+                    self.responses_channel
+                        .blocking_send((self.worker_id, response_message))
+                        .with_context(|| {
+                            format!(
+                                "Compute worker {} could not send response. This worker will stop.",
+                                self.worker_id.id
+                            )
+                        })?;
 
-            // block until the response is sent
-            self.responses_channel
-                .blocking_send((self.worker_id, response_message))
-                .with_context(|| {
-                    format!(
-                        "Compute worker {} could not send response. This worker will stop.",
-                        self.worker_id.id
-                    )
-                })?;
-
-            debug!("Worker {} sent his response.", self.worker_id.id);
+                    debug!("Worker {} sent his response.", self.worker_id.id);
+                }
+            }
         }
     }
 
