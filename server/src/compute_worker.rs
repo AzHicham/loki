1b36a62f36c823345ce7c7befa3fd9222fce6222
Pascal Benchimol <pascal.benchimol@kisio.com>
2022-01-19 10:32:47 +0100

    log an error message when a compute worker crash

--- server/src/compute_worker.rs
+++ server/src/compute_worker.rs
@@ -98,7 +98,12 @@ impl ComputeWorker {
         (result, requests_channel_sender)
     }
 
-    pub fn run(mut self) -> Result<(), Error> {
+    pub fn run(mut self) {
+        let run_result = self.run_loop();
+        error!("Worker {} stopped : {:#?}", &self.worker_id.id, run_result);
+    }
+
+    pub fn run_loop(&mut self) -> Result<(), Error> {
         info!("Worker {} has launched.", self.worker_id.id);
         loop {
             // block on receiving message
