c911b5bf3c90d95d521400a417e625a338729da6
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2022-07-18 11:46:44 +0200

    timer for status/metadatas requests

--- server/src/status_worker.rs
+++ server/src/status_worker.rs
@@ -47,7 +47,7 @@ use launch::loki::{
     NaiveDateTime,
 };
 
-use std::thread;
+use std::{thread, time::SystemTime};
 
 use crate::ServerConfig;
 use tokio::{runtime::Builder, sync::mpsc};
@@ -173,12 +173,9 @@ impl StatusWorker {
     }
 
     fn handle_request(&self, request_message: RequestMessage) -> Result<(), Error> {
+        let request_timer = SystemTime::now();
         let requested_api = request_message.payload.requested_api();
-        let request_id = request_message
-            .payload
-            .request_id
-            .clone()
-            .unwrap_or_default();
+        let request_id = request_message.payload.request_id.unwrap_or_default();
         debug!(
             "Status worker received request on api {:?} with id '{}'",
             requested_api, request_id
@@ -214,9 +211,13 @@ impl StatusWorker {
                 )
             })?;
 
+        let duration = request_timer.elapsed().map_or_else(
+            |err| err.to_string(),
+            |duration| duration.as_millis().to_string(),
+        );
         info!(
-            "Status worker responded to request on api {:?} with id '{}'",
-            requested_api, request_id
+            "Status worker responded in {} ms to request on api {:?} with id '{}'",
+            duration, requested_api, request_id
         );
 
         Ok(())
