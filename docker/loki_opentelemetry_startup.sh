e6a6c84c00c9a8c8b0eaf12c604036889eea430a
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2022-12-02 16:12:57 +0100

    new docker with metrics exported

---
+++ docker/loki_opentelemetry_startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start opentelemetry exporter
+/usr/bin/otelcol-contrib --config=/etc/otelcol/config.yaml &
+
+# Start loki
+/usr/local/bin/loki_server &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
